0f30a6932101819878f02013422fd4f79456a523
Ilie-Eduard-George <eduardgeo72@gmail.com>
2026-01-09 17:45:46 +0200

    Update Store.h

--- include/Store.h
+++ include/Store.h
@@ -1,1 +1,23 @@
+#pragma once
+#include "Inventory.h"
+#include "StocProdus.h"
+#include <stdexcept>
+
+class Store {
+private:
+    Inventory inventory;
+    int buget;
+public:
+    explicit Store(int initialBudget);
+
+    int getBuget() const;
+    const Inventory& getInventory() const;
+    Inventory& getInventory();
+
+    bool buyFromCatalog(const StocProdus& item);
+    int sellItem(size_t idx, int qty);
+    int sellAll();
+
+    friend std::ostream& operator<<(std::ostream& os, const Store& s);
+};
 
