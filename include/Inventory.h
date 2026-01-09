fdf7332a83a9f8c5576f0276b23a36253e2d1d46
Ilie-Eduard-George <eduardgeo72@gmail.com>
2026-01-09 17:44:02 +0200

    Update Inventory.h

--- include/Inventory.h
+++ include/Inventory.h
@@ -1,1 +1,28 @@
+#pragma once
+#include "StocProdus.h"
+#include <vector>
+#include <map>
+#include <optional>
+
+class Inventory {
+private:
+    std::vector<StocProdus> items;
+    std::map<std::string,int> indexByModel;
+public:
+    Inventory() = default;
+
+    void addItem(const StocProdus& s);
+    bool removeItemAt(size_t idx);
+
+    size_t size() const;
+    const StocProdus& at(size_t idx) const;
+    StocProdus& at(size_t idx);
+
+    double calcTotalPotentialProfit() const;
+    std::vector<const StocProdus*> findByBrand(const std::string& brand) const;
+    void sortByPotentialProfitDesc();
+    std::optional<size_t> findIndexByModel(const std::string& model) const;
+
+    friend std::ostream& operator<<(std::ostream& os, const Inventory& inv);
+};
 
