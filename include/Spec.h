97eec06002a8413d1114ee37a3e7bab4dd26a880
Ilie-Eduard-George <eduardgeo72@gmail.com>
2026-01-09 17:36:50 +0200

    Update Spec.h

--- include/Spec.h
+++ include/Spec.h
@@ -1,1 +1,22 @@
+#pragma once
+#include <string>
+#include <ostream>
+
+class Spec {
+private:
+    std::string Cpu;
+    int Ram;
+    int Stocare;
+public:
+    explicit Spec(const std::string& cpu = "", int ram = 0, int stocare = 0);
+    Spec(const Spec& other) = default;
+    Spec& operator=(const Spec& other) = default;
+    ~Spec() = default;
+
+    [[nodiscard]] const std::string& getCpu() const;
+    [[nodiscard]] int getRam() const;
+    [[nodiscard]] int getStocare() const;
+
+    friend std::ostream& operator<<(std::ostream& os, const Spec& s);
+};
 
