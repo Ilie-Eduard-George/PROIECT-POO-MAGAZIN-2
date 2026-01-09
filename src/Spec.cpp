4643e73ba28387ab67f61c955f6c185e0ae8529f
Ilie-Eduard-George <eduardgeo72@gmail.com>
2026-01-09 17:37:14 +0200

    Update Spec.cpp

--- src/Spec.cpp
+++ src/Spec.cpp
@@ -1,1 +1,15 @@
+#include "Spec.h"
+using namespace std;
+
+Spec::Spec(const string& cpu, int ram, int stocare)
+    : Cpu(cpu), Ram(ram), Stocare(stocare) {}
+
+const string& Spec::getCpu() const { return Cpu; }
+int Spec::getRam() const { return Ram; }
+int Spec::getStocare() const { return Stocare; }
+
+ostream& operator<<(ostream& os, const Spec& s) {
+    os << "CPU: " << s.Cpu << ", RAM: " << s.Ram << "GB, Storage: " << s.Stocare << "GB";
+    return os;
+}
 
