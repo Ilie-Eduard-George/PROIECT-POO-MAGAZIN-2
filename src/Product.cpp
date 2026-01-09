96ca43aba2f0b41f819fadc2d7ff6419f20eeaa2
Ilie-Eduard-George <eduardgeo72@gmail.com>
2026-01-09 17:39:10 +0200

    Update Product.cpp

--- src/Product.cpp
+++ src/Product.cpp
@@ -1,1 +1,17 @@
+#include "Product.h"
+using namespace std;
+
+Product::Product(const string& categorie, const string& brand, const string& model, const Spec& spec)
+    : categorie(categorie), brand(brand), model(model), spec(spec) {}
+
+const string& Product::getCategorie() const { return categorie; }
+const string& Product::getBrand() const { return brand; }
+const string& Product::getModel() const { return model; }
+const Spec& Product::getSpec() const { return spec; }
+
+ostream& operator<<(ostream& os, const Product& p) {
+    os << p.categorie << " " << p.brand << " " << p.model << " - " << p.spec;
+    return os;
+}
+
 
