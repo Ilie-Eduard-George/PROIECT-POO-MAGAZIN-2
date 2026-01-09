0118b7bb8e4e14f4b3bb8a127f56d24728a2f8c9
Ilie-Eduard-George <eduardgeo72@gmail.com>
2026-01-09 17:40:28 +0200

    Update CatalogItem.cpp

--- src/CatalogItem.cpp
+++ src/CatalogItem.cpp
@@ -1,1 +1,15 @@
+#include "CatalogItem.h"
+using namespace std;
+
+CatalogItem::CatalogItem(const Product& product, int pret, const string& culoare)
+    : product(product), pret(pret), culoare(culoare) {}
+
+const Product& CatalogItem::getProduct() const { return product; }
+int CatalogItem::getPret() const { return pret; }
+const string& CatalogItem::getCuloare() const { return culoare; }
+
+ostream& operator<<(ostream& os, const CatalogItem& c) {
+    os << c.product << " " << c.culoare << ", pret: " << c.pret << " lei";
+    return os;
+}
 
