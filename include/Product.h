de122761bb6c8abc8f9b3b17ff138aa132aceb4c
Ilie-Eduard-George <eduardgeo72@gmail.com>
2026-01-09 17:38:42 +0200

    Update Product.h

--- include/Product.h
+++ include/Product.h
@@ -1,1 +1,25 @@
+#pragma once
+#include "Spec.h"
+#include <string>
+#include <ostream>
 
+class Product {
+private:
+    std::string categorie;
+    std::string brand;
+    std::string model;
+    Spec spec;
+public:
+    explicit Product(const std::string& categorie = "", const std::string& brand = "",
+                     const std::string& model = "", const Spec& spec = Spec());
+    Product(const Product& other) = default;
+    Product& operator=(const Product& other) = default;
+    ~Product() = default;
+
+    [[nodiscard]] const std::string& getCategorie() const;
+    [[nodiscard]] const std::string& getBrand() const;
+    [[nodiscard]] const std::string& getModel() const;
+    [[nodiscard]] const Spec& getSpec() const;
+
+    friend std::ostream& operator<<(std::ostream& os, const Product& p);
+};
