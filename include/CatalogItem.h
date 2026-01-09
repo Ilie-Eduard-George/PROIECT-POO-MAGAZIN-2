365ef94082915d977a2d4c6ad3f13120a2263280
Ilie-Eduard-George <eduardgeo72@gmail.com>
2026-01-09 17:39:51 +0200

    Update CatalogItem.h

--- include/CatalogItem.h
+++ include/CatalogItem.h
@@ -1,1 +1,23 @@
+#pragma once
+#include "Product.h"
+#include <string>
+#include <ostream>
+
+class CatalogItem {
+private:
+    Product product;
+    int pret;
+    std::string culoare;
+public:
+    explicit CatalogItem(const Product& product, int pret, const std::string& culoare);
+    CatalogItem(const CatalogItem& other) = default;
+    CatalogItem& operator=(const CatalogItem& other) = default;
+    ~CatalogItem() = default;
+
+    [[nodiscard]] const Product& getProduct() const;
+    [[nodiscard]] int getPret() const;
+    [[nodiscard]] const std::string& getCuloare() const;
+
+    friend std::ostream& operator<<(std::ostream& os, const CatalogItem& c);
+};
 
